1f9ec647411764a47ef1549fa91d4f534605117b
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-03-19 23:38:47 +0200

    Third commit

--- src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -3,7 +3,7 @@ package com.appsdeveloperblog.app.ws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  //Annotation
 public class MobileAppWsApplication {
 
 	public static void main(String[] args) {
